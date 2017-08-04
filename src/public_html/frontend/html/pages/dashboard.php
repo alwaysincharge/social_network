6957e9259f83d3abdfee51d23e361e9368ed8c2d
Atsu Davoh <atsunewjoint@gmail.com>
2017-08-04 09:03:35 +0300

    Began work on chat system.

---
+++ src/public_html/frontend/html/pages/dashboard.php
@@ -0,0 +1,741 @@
+
+
+<html lang="en">
+    
+    
+    
+    
+<head>
+    
+    
+	<title>Friday Camp - connect with people, you already know.</title>
+    
+    <meta name="description" content="Create, display and update your resume, find jobs, find a co-founder, message your hero, meet other techies, all here.">
+    
+    <?php include('../templates/head_info.php'); ?>
+    
+    
+</head>
+    
+    
+    
+    
+    
+<body class="dashboard-body">
+    
+    
+    
+    
+   <nav class="nav-head">
+    
+    <div class="row nav-main-row">
+        
+        
+        <div class="col-xs-6">
+            
+            <a class="logo-heading-1">friday camp <span class="logo-heading-2">//</span> <span class="logo-heading-3">physics group work</span></a>
+                            
+        </div>
+        
+        
+        
+        
+        <div class="col-xs-6">
+            
+            
+            <input maxlength="100" name="keywords" class="search-main" placeholder="Search this group" />
+                
+            
+            
+            
+            <a>
+                
+            <button class="btn new-group-1">   
+                
+            <img src="pencil.svg" width="20" height="20" class="new-group-2"  />
+                    
+            Create new group</button>
+            
+            </a>
+            
+            
+            
+            
+            <img src="a.jpg" width="35" height="35" class="current-user-img"  />
+            
+                    
+        </div>
+        
+        
+        
+       </div>
+    
+    </nav>
+
+                    
+    
+    
+    
+    
+            <div class="row main-body-div">
+                
+                
+                
+                <div class="col-xs-9">
+                
+                
+                <div>
+                    
+                
+                <div class="col-xs-1" style="">
+                    
+                    
+                <div>
+                    
+                     
+                    <img src="a.jpg" width="70" height="70" class="writer-profile-img"  />
+                    
+                   
+                    
+                </div>    
+                
+                
+                </div>
+                
+                
+                
+                
+                <div class="col-xs-11">
+                    
+                    
+                    <div class="row post-editor">
+                        
+                    
+                        
+                        
+                    <div class="col-xs-2">
+                        
+                        <img src="chatting.svg" class="input-style-1"  />
+                        
+                        <p class="input-style-2">Chat</p>
+                        
+                    </div>
+                        
+                        
+                        
+                        
+                      
+                    <div class="col-xs-2">
+                        
+                         <img src="typography.svg" class="input-style-1"  />
+                        
+                         <p class="input-style-2">Text post</p>
+                        
+                    </div>
+                        
+                        
+                        
+                        
+                    <div class="col-xs-2">
+                        
+                        <img src="camera.svg" class="input-style-1"  />
+                        
+                         <p class="input-style-2">Photo/Album</p>
+                        
+                    </div>
+                        
+                        
+                        
+                        
+                        
+                    <div class="col-xs-2">
+                        
+                        <img src="video-camera.svg" class="input-style-1" />
+                        
+                         <p class="input-style-2">Videos</p>
+                        
+                    </div>
+                        
+                        
+                        
+                        
+                        
+                        
+                    <div class="col-xs-2">
+                        
+                        <img src="hands.svg" class="input-style-1"  />
+                        
+                         <p class="input-style-2">Polls</p>
+                        
+                    </div>
+                        
+                        
+                       
+                        
+                        <div class="col-xs-2">
+                        
+                        <img src="controls.svg" class="input-style-1"  />
+                        
+                         <p class="input-style-2">Settings</p>
+                        
+                    </div>
+                        
+                        
+                    
+                    </div>
+                    
+                    
+                  
+                    
+                        <textarea id="name" type="text" class="search-main" placeholder="write something" style="margin-bottom: 10px; width: 100%; height: 100px; resize: none; outline: none;"></textarea><br>
+                     
+                    
+                        <a><img src="attachment.svg" style="width: 20px;"  /></a>
+                    
+                        <a><img src="photo-camera.svg" style="width: 20px; margin-left: 10px;"  /></a>
+                    
+                        <a><img src="cross.svg" style="width: 20px; margin-left: 10px;"  /></a>
+                    
+                      <!--  <input id="submit" class="btn new-group-1" type="submit" style="margin-left: 0px;" value="POST"> --><br><br><br><br><br><br><br><br><br>
+                    
+                
+                    
+                
+                </div>
+                
+                    
+                    
+                </div>
+                    
+                    
+                    
+                    
+                    
+                    
+                    <div id="wrapper">
+                        
+                        
+                        
+                             <div id="areas2">
+                        
+                        
+                        
+                        </div>                               
+                        
+                        
+                        
+                        <div id="areas">
+                        
+                        
+                        
+                        </div>
+                    
+                    
+
+                    </div>
+                   
+
+                    
+                <div>
+                    
+                    
+                    
+                    
+                  
+                <div class="col-xs-1">
+                    
+                    
+                <div>
+                    
+                     <img src="a.jpg" width="70" height="70" class="post-owner-img" />
+                    
+                     
+                    
+                </div>    
+                
+                
+                </div>
+                
+                
+                
+                
+                <div class="col-xs-11">
+                    
+                   
+                    <p class="post-username">bobdylan</p>
+                    
+                
+                    <img src="tumblr_oonx42GBY31tl2cbeo1_500.jpg" class="post-img-1" />
+                
+                </div>
+                
+                  
+                    
+                    
+                </div>  
+                    
+                 
+                    
+                    
+                    
+                    
+                            
+                    
+                <div>
+                    
+                  
+                <div class="col-xs-1">
+                    
+                    
+                <div>
+                    
+                     <img src="a.jpg" width="70" height="70" class="post-owner-img"  />
+                    
+                     
+                    
+                </div>    
+                
+                
+                </div>
+                
+                
+                
+                
+                <div class="col-xs-11" >
+                    
+                   
+                    <p class="post-username">atsunewjoint</p>
+                    
+                
+                    <img src="DF3UvKNVoAA9riU.jpg" class="post-img-1"  />
+                
+                </div>
+                
+                  
+                    
+                    
+                </div>  
+                    
+                 
+                    <h1>gooo</h1>
+                      
+                        
+                
+                
+                </div>
+                
+                
+            
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                <div class="col-xs-3 group-list-div">
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                  <div class="row group-list-row">
+                        
+                        
+                        <div class="col-xs-2">
+                            
+                           <img src="a.jpg" width="40" height="40" style="group-list-profile-img"  />
+                        
+                        </div>
+                        
+                      
+                        
+                        <div class="col-xs-7 group-list-body">
+                            
+                            <a class="group-list-name">Family</a><br>                            
+                            <a class="group-list-membercount">90 members</a>
+                        
+                        </div>
+                        
+                        
+                        
+                        <div class="col-xs-3 group-list-notif-1">
+                            
+                            <img src="view.svg" width="30" height="30" style="group-list-notif-2"  />
+                            
+                        </div>
+                        
+                        
+                    </div>
+                    
+                    
+                    
+                    
+                    
+                    
+                  <div class="row group-list-row">
+                        
+                        
+                        <div class="col-xs-2">
+                            
+                           <img src="a.jpg" width="40" height="40" style="group-list-profile-img"  />
+                        
+                        </div>
+                        
+                      
+                        
+                        <div class="col-xs-7 group-list-body">
+                            
+                            <a class="group-list-name">Family</a><br>                            
+                            <a class="group-list-membercount">90 members</a>
+                        
+                        </div>
+                        
+                        
+                        
+                        <div class="col-xs-3 group-list-notif-1">
+                            
+                            <a class="group-list-notifs">76+</a>
+                            
+                        </div>
+                        
+                        
+                    </div>
+                        
+                    
+                    
+                    
+                    
+                  
+                    
+                              
+                  <div class="row group-list-row">
+                        
+                        
+                        <div class="col-xs-2">
+                            
+                           <img src="a.jpg" width="40" height="40" style="group-list-profile-img"  />
+                        
+                        </div>
+                        
+                      
+                        
+                        <div class="col-xs-7 group-list-body">
+                            
+                            <a class="group-list-name">Family</a><br>                            
+                            <a class="group-list-membercount">90 members</a>
+                        
+                        </div>
+                        
+                        
+                        
+                        <div class="col-xs-3 group-list-notif-1">
+                            
+                            <a class="group-list-notifs">76+</a>
+                            
+                        </div>
+                        
+                        
+                    </div>
+                        
+                    
+                    
+                    
+                    
+                    
+                    
+               
+                
+                </div>
+                
+            </div>
+    
+    
+    
+</body>
+    
+ 
+    
+</html>
+
+
+<script type="text/javascript" src="jquery-3.2.1.min.js"></script>
+    
+
+
+
+<script type="text/javascript">
+    
+     
+    
+
+    
+    
+    // When the user reaches the bottom, get more data from the database.
+      $(window).on("scroll", function() {
+          
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                
+              
+                displayFromDatabasePagination();
+                
+                
+            }
+          
+        });
+    
+    
+    
+    
+    
+    //When you click the chat box...
+    $('#name').on("focus", function(){
+        
+        // When you click eneter...
+        $('#name').keypress(function(e) {
+            
+     
+            
+        if(e.which == 13) {
+        
+            
+        
+        var name = $("#name").val();
+            var nnn = $("#name").val().trim();
+            
+            if (nnn != "") {
+            
+             $.ajax({
+        
+             url: "ajax.php",
+             type: "POST",
+             async: false,
+             data: {
+                 
+               "done": 1,
+               "name": name
+            
+             }, 
+            
+             success: function(data) {
+                 
+                $("#name").val(""); 
+               
+            
+             }
+        
+             }); } else {
+                 
+                  $("#name").val(""); 
+                 
+             }
+            
+            return e.which !== 13;
+        
+        
+    }
+});
+   //do some stuff
+          
+});
+    
+    
+    var lastTimeID = 0;  
+    
+     var firstTimeID = 0;  
+    
+    $(document).ready(function(){
+        
+        
+      displayFromDatabasePagination();
+        
+        
+        startPostLoop();
+            
+
+        // Submit the chat data to database.
+        $("#submit").click(function(){
+        
+           
+        
+        });
+    
+      
+    
+        
+    });
+    
+    
+   
+    
+    
+    
+   function startPostLoop(){
+    
+       displayFromDatabase();                       
+                           
+   }
+    
+    
+    
+    
+    
+    // Fetch chat data from the database in chunks.
+    function displayFromDatabase(){
+        
+        var name = $("#name").val();
+            
+        $.ajax({
+        
+        url: "ajax.php",
+        type: "POST",
+        async: false,
+        data: {
+            
+            "melted": 1,
+            "laminate": lastTimeID
+                
+        }, 
+            
+        success: function(d) {
+            
+            
+            var jsonData = JSON.parse(d);
+            
+            var jsonLength = jsonData.allposts.length;
+            
+            var html = "";
+            
+            var tsutsus;
+            
+            if (lastTimeID === 0) {
+                
+                tsutsus = 1;
+                
+            }
+            
+            
+            for (var i = 0; i < jsonLength; i++) {
+                
+              var result = jsonData.allposts[i];
+                
+              lastTimeID = result.id;
+                
+              html += '<div><p>(' + result.id + ') ' + result.name + '</p></div>';
+      
+            }
+      
+
+            if (tsutsus === 1) {
+                
+                tsutsus = 3;
+                
+            } else {
+                
+             $('#areas2').prepend(html);   
+            }
+          
+            
+    
+            setTimeout(startPostLoop, 500); 
+            
+            
+        }
+        
+        
+        });
+        
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        function displayFromDatabasePagination(){
+        
+    
+            
+        $.ajax({
+        
+        url: "ajax.php",
+        type: "POST",
+        async: false,
+        data: {
+            
+            "felted": 1,
+            "laminate": firstTimeID
+                
+        }, 
+            
+        success: function(dd) {
+            
+            
+            var jsonData2 = JSON.parse(dd);
+            
+            var jsonLength2 = jsonData2.allposts1.length;
+            
+            var html2 = "";
+            
+            
+           for (var i2 = 0; i2 < jsonLength2; i2++) {
+                
+              var result2 = jsonData2.allposts1[i2];
+                
+              firstTimeID = result2.id;
+                
+              html2 += '<div><p style=\"background: red;\">(' + result2.id + ') ' + result2.name + '</p></div>';
+      
+           }
+      
+        //  alert(dd);
+          
+          $('#areas2').append(html2);
+    
+            
+            
+            
+        }
+        
+        
+        });
+        
+        }
+    
+    
+    
+
+
+ 
+    
+</script>   
